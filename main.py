7aa5a1584e143fbc3e38402c81af386721bfb44e
ICBF-Analitica <79280614+ICBF-Analitica@users.noreply.github.com>
2024-11-15 13:37:34 -0500

    Update main.py
    quitando libreria nltk

--- main.py
+++ main.py
@@ -277,7 +277,7 @@ noticias_colombia.to_excel('Data/consolidado_noticias.xlsx', index=False)
 noticias_colombia.columns
 #<h1>TOKENIZACIÓN</h1>
 import pandas as pd
-import nltk
+
 nltk.download('stopwords')
 nltk.download('punkt')
 from nltk.tokenize import word_tokenize
