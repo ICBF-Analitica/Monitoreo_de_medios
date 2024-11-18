404594dff33c88d7b1db92880b2acdd09f271563
ICBF-Analitica <79280614+ICBF-Analitica@users.noreply.github.com>
2024-11-18 16:24:08 -0500

    Update main.py
    ajust

--- main.py
+++ main.py
@@ -280,7 +280,7 @@ import pandas as pd
 import nltk
 
 nltk.download('stopwords')
-#nltk.download('punkt')
+nltk.download('punkt')
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 
